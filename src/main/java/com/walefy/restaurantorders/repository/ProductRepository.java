0057d893426824edc1f9c1420d64cf94b195fbc1
walefy <walefyd1@gmail.com>
2024-07-07 16:26:51 -0300

    feat: implements crud in ProductService

--- src/main/java/com/walefy/restaurantorders/repository/ProductRepository.java
+++ src/main/java/com/walefy/restaurantorders/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.walefy.restaurantorders.repository;
 
 import com.walefy.restaurantorders.entity.Product;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+  List<Product> findAllByName(String name);
 }
--- src/main/java/com/walefy/restaurantorders/service/ProductService.java
+++ src/main/java/com/walefy/restaurantorders/service/ProductService.java
@@ -2,7 +2,9 @@ package com.walefy.restaurantorders.service;
 
 import com.walefy.restaurantorders.dto.ProductCreateDto;
 import com.walefy.restaurantorders.entity.Product;
+import com.walefy.restaurantorders.exception.ProductNotFoundException;
 import com.walefy.restaurantorders.repository.ProductRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,5 +19,22 @@ public class ProductService {
 
   public Product create(ProductCreateDto productCreate) {
     return this.productRepository.save(productCreate.toEntity());
+  }
+
+  public List<Product> findAll() {
+    return this.productRepository.findAll();
+  }
+
+  public Product findById(Long id) throws ProductNotFoundException {
+    return this.productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException(id));
+  }
+
+  public List<Product> findByName(String name) {
+    return this.productRepository.findAllByName(name);
+  }
+
+  public void delete(Long id) throws ProductNotFoundException {
+    Product product = this.findById(id);
+    this.productRepository.delete(product);
   }
 }
