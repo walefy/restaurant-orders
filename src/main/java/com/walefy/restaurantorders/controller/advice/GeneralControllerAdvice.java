e958e58f2ed45b6dca3c09a60241fc05f65be0dc
walefy <walefyd1@gmail.com>
2024-07-16 13:19:53 -0300

    feat: add controller advice

---
+++ src/main/java/com/walefy/restaurantorders/controller/advice/GeneralControllerAdvice.java
@@ -0,0 +1,26 @@
+package com.walefy.restaurantorders.controller.advice;
+
+
+import com.walefy.restaurantorders.exception.NotFoundException;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GeneralControllerAdvice {
+  @ExceptionHandler({ NotFoundException.class })
+  public ResponseEntity<Map<String, String>> handleNotFound(NotFoundException e) {
+    Map<String, String> response = Map.of("message", e.getMessage());
+
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+  }
+
+  @ExceptionHandler({ Exception.class })
+  public ResponseEntity<Map<String, String>> handleGeneric(Exception e) {
+    Map<String, String> response = Map.of("message", e.getMessage());
+
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+  }
+}
